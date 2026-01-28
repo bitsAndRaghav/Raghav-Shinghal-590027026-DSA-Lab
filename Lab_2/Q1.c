3ed30691ed1e378df2f9900e51bbeabe57d6c3b5
bitsAndRaghav <raghavshinghal2007@gmail.com>
2026-01-28 11:33:05 +0530

    27-Jan-2026

--- Lab_2/Q1.c
+++ Lab_2/Q1.c
@@ -3,35 +3,33 @@
 
 int main()
 {
-    int a[20][3];   // [student][sub1, sub2, total]
-    int i;
+    int a[20][3] ,i;
 
-    for(i = 0; i < 20; i++)
+    for(i=0; i<20; i++)
     {
-        printf("\nEnter marks for Student %d\n", i + 1);
+        printf("Enter marks for Student %d\n", i + 1);
 
-        // Input Subject 1
-        do {
+        do 
+        {
             printf("Subject 1: ");
             scanf("%d", &a[i][0]);
         } while(a[i][0] < 0 || a[i][0] > 100);
 
-        // Input Subject 2
-        do {
+        do 
+        {
             printf("Subject 2: ");
             scanf("%d", &a[i][1]);
         } while(a[i][1] < 0 || a[i][1] > 100);
 
-        // Calculate total
         a[i][2] = a[i][0] + a[i][1];
     }
 
-    // Display table
-    printf("\n\nStudent\tSub1\tSub2\tTotal(/200)\n");
+    printf("\n\nTable of Marks:\n");
+    printf("\nStudent\t\tSub1\tSub2\tTotal(/200)\n");
 
     for(i = 0; i < 20; i++)
     {
-        printf("S%d\t%d\t%d\t%d\n",
+        printf("STUDENT%d\t%d\t%d\t%d\n",
                i + 1, a[i][0], a[i][1], a[i][2]);
     }
 
